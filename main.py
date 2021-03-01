62fad73b6a21fa2b4a99198a16fc52d36cc0412c
bibhabasumohapatra <68384968+bibhabasumohapatra@users.noreply.github.com>
2021-03-01 20:05:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+#importing libraries
 import mysql.connector  
 
-con = mysql.connector.connect(host = 'localhost',user = 'bm',password = 'gulu1610',database = 'motor_parts_shop' )
 
-print(con)
+
+
+#main class of db
+class motor_parts_DB:
+
+    def __init__(self):
+        
+        self.con = mysql.connector.connect(host = 'localhost',user = 'bm',password = 'gulu1610',database = 'motor_parts_shop' )
+
+    
+
+    #def_insert inserts data into databases 
+    def insert_cust_detail(self,cust_name ,car,part_damaged,date_entry ,date_comp ,payment):
+        query = "insert into cust_detail(cust_name ,car,part_damaged,date_entry ,date_comp ,payment) values('{}','{}','{}','{}','{}',{})".format(cust_name ,car,
+                                                                                                                                                 part_damaged,date_entry,
+                                                                                                                                                 date_comp ,payment)
+
+        print(query)
+        cur = self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+        print("inserted data to db")
+
+
+
+
+
+
+
+
+#declaring definations
+motor_db = motor_parts_DB()
+#motor_db.insert_cust_detail("bibhabasu","tesla","tyres","2nd feb","22nd feb",22000)
