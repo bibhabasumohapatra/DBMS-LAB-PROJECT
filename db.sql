712ebb7be702386876b135b42c62b03b1777dc16
bibhabasumohapatra <68384968+bibhabasumohapatra@users.noreply.github.com>
2021-03-01 20:16:57 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ create table if not exists cust_detail(
    id int primary key auto_increment,
    cust_name varchar(64) not null,
    car varchar(32),
-   part_damaged varchar(32);
+   part_damaged varchar(32),
    date_of_service varchar(32) not null,
    date_comp varchar(32) not null,
    payment int(10)
