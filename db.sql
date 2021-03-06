1f9763673742b7f8487cf30fe2aa23131cd868bc
bibhabasumohapatra <68384968+bibhabasumohapatra@users.noreply.github.com>
2021-03-06 19:22:11 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,10 +1,10 @@
 create table if not exists cust_detail(
-   id int primary key auto_increment,
+   token_id int primary key auto_increment,
    cust_name varchar(64) not null,
    car varchar(32),
    part_damaged varchar(32),
-   date_of_service varchar(32) not null,
-   date_comp varchar(32) not null,
+   date_entry varchar(32) not null,
+   date_comp varchar(32),
    payment int
 );
 
@@ -13,16 +13,14 @@ create table if not exists stock_spare_parts(
    id int primary key auto_increment,
    parts_name varchar(16),
    parts_available int,
-   price int
-   
+   parts_required int,
+   price int  
 );
 
 create table if not exists emp_details(
-   emp_id int primary key,
+   emp_id int primary key auto_increment,
    emp_name varchar(16),
-   emp_mobile int,
-   emp_address int,
+   emp_address varchar(16),
    emp_department varchar(16),
-   current_salary int,
-   no_leaves int,
+   current_salary int
 );
